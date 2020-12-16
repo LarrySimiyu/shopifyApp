edb39e3f4d391570d38a7632441c9a82da69b440
LarrySimiyu <larry.simiyu@gmail.com>
2020-12-15 19:13:27 -0800

    app installed succesfully on shopify

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2daa7f376ca8a182c47d";
+/******/ 	var hotCurrentHash = "26217bca2fa98e3d3b50";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
